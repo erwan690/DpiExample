6b027f01f36981c98a869d5b8b0cabd256ecd2b2
akse <erwanakse@gmail.com>
2017-07-05 17:07:11 +0700

    sheet 3 project

---
+++ app/src/main/java/studio/akse/dpiexample/DetailProjectActivity.java
@@ -0,0 +1,13 @@
+package studio.akse.dpiexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_project);
+    }
+}
--- app/src/main/java/studio/akse/dpiexample/ProjectActivity.java
+++ app/src/main/java/studio/akse/dpiexample/ProjectActivity.java
@@ -1,7 +1,8 @@
 package studio.akse.dpiexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 public class ProjectActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class ProjectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
         setTitle("Project");
+
+        TabHost host = (TabHost) findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Participated");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Not Participated");
+        host.addTab(spec);
+
+
     }
 }
