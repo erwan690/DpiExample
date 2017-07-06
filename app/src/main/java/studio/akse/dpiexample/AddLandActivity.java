412c7f23f18f8d9b521e2a478a69e2729bd6ddd3
akse <erwanakse@gmail.com>
2017-07-07 01:28:29 +0700

    add maps click function

--- app/src/main/java/studio/akse/dpiexample/AddLandActivity.java
+++ app/src/main/java/studio/akse/dpiexample/AddLandActivity.java
@@ -2,13 +2,57 @@ package studio.akse.dpiexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
-public class AddLandActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class AddLandActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+    private double latitude, longitude;
+    private EditText lat, longt;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_land);
         setTitle(" Create Land Listing");
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapadd);
+        mapFragment.getMapAsync(this);
+        lat = (EditText) findViewById(R.id.latadd);
+        longt = (EditText) findViewById(R.id.longadd);
+
+
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                latitude = latLng.latitude;
+                longitude = latLng.longitude;
+                lat.setText(String.valueOf(latitude));
+                longt.setText(String.valueOf(longitude));
+
+                if (marker != null) {
+                    marker.remove();
+                }
+                marker = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).draggable(true).visible(true));
+            }
+        });
+
+    }
+
+
 }
