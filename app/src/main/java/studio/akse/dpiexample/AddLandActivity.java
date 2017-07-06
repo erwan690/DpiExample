da546d73a3a54d8df36c8c4aa871cc0a4ab00669
akse <erwanakse@gmail.com>
2017-07-06 22:32:43 +0700

    land progress

---
+++ app/src/main/java/studio/akse/dpiexample/AddLandActivity.java
@@ -0,0 +1,13 @@
+package studio.akse.dpiexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddLandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_land);
+    }
+}
