a8ade95c3be765ccaac918c97a78d1122b92e1e0
akse <erwanakse@gmail.com>
2017-07-06 21:37:10 +0700

    change login ui

--- app/src/main/java/studio/akse/dpiexample/LandActivity.java
+++ app/src/main/java/studio/akse/dpiexample/LandActivity.java
@@ -1,7 +1,8 @@
 package studio.akse.dpiexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 public class LandActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class LandActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_land);
         setTitle("Land Listing");
+
+        TabHost host = (TabHost) findViewById(R.id.tabhost_land);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1_land);
+        spec.setIndicator("All");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2_land);
+        spec.setIndicator("My Listing");
+        host.addTab(spec);
     }
 }
--- app/src/main/java/studio/akse/dpiexample/LoginActivity.java
+++ app/src/main/java/studio/akse/dpiexample/LoginActivity.java
@@ -3,22 +3,20 @@ package studio.akse.dpiexample;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -67,7 +65,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        setTitle("Login");
+        //setTitle("Login");
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
