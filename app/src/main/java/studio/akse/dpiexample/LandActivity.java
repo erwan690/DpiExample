0b0d96726c8a322cdbca8f157fab5146ea88a1a4
akse <erwanakse@gmail.com>
2017-07-06 22:45:04 +0700

    progress add land

--- app/src/main/java/studio/akse/dpiexample/LandActivity.java
+++ app/src/main/java/studio/akse/dpiexample/LandActivity.java
@@ -1,16 +1,30 @@
 package studio.akse.dpiexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TabHost;
 
 public class LandActivity extends AppCompatActivity {
+
+    private FloatingActionButton add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_land);
         setTitle("Land Listing");
+
+        add = (FloatingActionButton) findViewById(R.id.add_tet);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent main = new Intent(LandActivity.this, AddLandActivity.class);
+                startActivity(main);
+            }
+        });
 
         TabHost host = (TabHost) findViewById(R.id.tabhost_land);
         host.setup();
